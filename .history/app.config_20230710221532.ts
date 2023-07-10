46a9ae1a62a06f5a97689038429c2c948c7558e4
Atle Stromsvold <obsfeil@outlook.com>
2023-07-10 22:20:51 +0200

    v

---
+++ .history/app.config_20230710221532.ts
@@ -0,0 +1,44 @@
+import { defineNuxtConfig } from 'nuxt/config'
+
+export default defineAppConfig({
+  docus: {
+    title: 'Docus',
+    description: 'The best place to start your documentation.',
+    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    socials: {
+      twitter: 'nuxt_js',
+      github: 'nuxt-themes/docus',
+      nuxt: {
+        label: 'Nuxt',
+        icon: 'simple-icons:nuxtdotjs',
+        href: 'https://nuxt.com'
+      }
+    },
+    github: {
+      dir: '.starters/default/content',
+      branch: 'main',
+      repo: 'docus',
+      owner: 'nuxt-themes',
+      edit: true
+    },
+    aside: {
+      level: 0,
+      collapsed: false,
+      exclude: []
+    },
+    main: {
+      padded: true,
+      fluid: true
+    },
+    header: {
+      logo: true,
+      showLinkIcon: true,
+      exclude: [],
+      fluid: true
+    }
+  }
+})
+
+function defineAppConfig(arg0: { docus: { title: string; description: string; image: string; socials: { twitter: string; github: string; nuxt: { label: string; icon: string; href: string } }; github: { dir: string; branch: string; repo: string; owner: string; edit: boolean }; aside: { level: number; collapsed: boolean; exclude: undefined[] }; main: { padded: boolean; fluid: boolean }; header: { logo: boolean; showLinkIcon: boolean; exclude: undefined[]; fluid: boolean } } }) {
+throw new Error('Function not implemented.')
+}
---
+++ .history/nuxt.config_20230710212537.ts
@@ -0,0 +1,18 @@
+export default defineNuxtConfig({
+  // https://github.com/nuxt-themes/docus
+  extends: '@nuxt-themes/docus',
+
+  modules: [
+    https://github.com/nuxt-modules/plausible
+    '@nuxtjs/plausible',
+    https://github.com/nuxt/devtools
+    '@nuxt/devtools'
+  ]
+})
+
+function defineNuxtConfig(arg0: {
+// https://github.com/nuxt-themes/docus
+extends: string; modules: string[];
+}) {
+throw new Error("Function not implemented.");
+}
