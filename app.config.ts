1a84d80a35a31b13c4ab273a38c6faaacc552e1d
Atle Stromsvold <obsfeil@outlook.com>
2023-07-11 19:10:28 +0200

    ff

--- app.config.ts
+++ app.config.ts
@@ -53,3 +53,7 @@ export default defineAppConfig({
   }
 })
 
+
+function defineAppConfig(arg0: { docus: { title: string; description: string; url: string; image: string; socials: { twitter: string; github: string; }; github: { root: string; edit: boolean; contributors: boolean; }; layout: string; aside: { level: number; filter: undefined[]; }; header: { title: boolean; logo: boolean; showLinkIcon: boolean; }; footer: { credits: { icon: string; text: string; href: string; }; textLinks: { text: string; href: string; target: string; rel: string; }[]; iconLinks: { label: string; href: string; component: string; }[]; }; }; }) {
+throw new Error("Function not implemented.");
+}
